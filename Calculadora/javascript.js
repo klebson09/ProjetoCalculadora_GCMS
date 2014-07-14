f9eee166dababd49c56bb5a02ce7c3467da87e52
Tassiors <tassiors@gmail.com>
2014-07-14 18:27:07 -0300

    Update JavaScript

--- Calculadora/javascript.js
+++ Calculadora/javascript.js
@@ -10,10 +10,13 @@ function lowTemperature(){
      c-=1;
      f-=1.8;
   
-     document.getElementById("kimg").width= k;
-     document.getElementById("cimg").width= c;
-     document.getElementById("fimg").width= f;
   }
+  
+  document.getElementById("kvisor").value = k;
+  document.getElementById("cvisor").value = c;
+  document.getElementById("fvisor").value = f.toFixed(2);
+  
+  alterarWidth();
 }
 
 function addTemperature(){
@@ -24,13 +27,69 @@ function addTemperature(){
      k+=1;
      c+=1;
      f+=1.8;
-  
-     document.getElementById("kimg").width= k;
-     document.getElementById("cimg").width= c;
-     document.getElementById("fimg").width= f;
   }
+  
+  document.getElementById("kvisor").value = k;
+  document.getElementById("cvisor").value = c;
+  document.getElementById("fvisor").value = f.toFixed(2);
+  
+  alterarWidth();
 }
 
+function calcularTemp(){
+  var n = document.getElementById("calcular").value;
+  var t = document.getElementById("selecionar").value;
+   
+  if(n>=273 && n<=373 && t=="K"){
+     
+	 document.getElementById("kvisor").value = n;
+	 k = n;
+	 
+	 document.getElementById("cvisor").value = parseFloat(n) - 273;
+	 c = n - 273;
+	 
+	 document.getElementById("fvisor").value = (((9/5)*(parseFloat(n)-273))+32).toFixed(2);
+	 f = (((9/5)*(parseFloat(n)-273))+32).toFixed(2);
+	 
+	 alterarWidth();
+	 return "";
+    }
+  
+  if(n>=0 && n<=100 && t=="C"){
+     
+	 document.getElementById("cvisor").value = n;
+	 c = n;
+	 
+	 document.getElementById("kvisor").value = parseFloat(n)+273;
+	 k = n + 273;
+	 
+	 document.getElementById("fvisor").value = ((9/5)*parseFloat(n)+32).toFixed(2);
+	 f = ((9/5)*parseFloat(n)+32).toFixed(2);
+	 alterarWidth();
+	 return "";
+    }
+     
+  if(n>=32 && n<=212 && t=="F"){
+     
+	 document.getElementById("fvisor").value = n;
+	 f = n;
+	 
+	 document.getElementById("kvisor").value = (5/9*(parseFloat(n)-32)+273).toFixed(2);
+	 k = (5/9*(n-32)+273).toFixed(2);
+	 
+	 document.getElementById("cvisor").value = (5/9*(parseFloat(n)-32)).toFixed(2);
+	 c = (5/9*(n-32)).toFixed(2);
+	 alterarWidth();
+     return "";
+  }
+   alert("Valor Inválido");  
+}
+
+function alterarWidth(){
+   document.getElementById("kimg").width= k;
+   document.getElementById("cimg").width= c;
+   document.getElementById("fimg").width= f;
+}
 
 function concatenar(numero){
   Calculadora.visor.value += numero;
@@ -81,5 +140,9 @@ function init(){
   document.getElementById("cimg").width=c;
   document.getElementById("fimg").height="10";
   document.getElementById("fimg").width=f;
+  
+  document.getElementById("kvisor").value = k;
+  document.getElementById("cvisor").value = c;
+  document.getElementById("fvisor").value = f;
 }
 onload = init;
